7c3e92994d4c9d6a30daa42a4560ffac17a0e92f
swilson-teladoc <sawyer.wilson@teladoc.com>
2021-06-28 02:57:18 +0000

    added validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,7 @@
 class Person < ApplicationRecord
     has_many :posts, dependent: :destroy
+
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :bio, presence: true, length: { in: 10..500 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :person
+
+  validates :subject, presence: true
+  validates :body, presence: true, length: { minimum: 10}
+  validates :date, presence: true
 end
